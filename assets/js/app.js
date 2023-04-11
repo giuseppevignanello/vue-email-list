c395cf60ea4fdd4a34868edcac84ba0874467173
giuseppevignanello <peppe.vignanello@gmail.com>
2023-04-11 16:25:39 +0200

    add number of emails selector

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,18 +3,24 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
+            numberOfEmail: 0,
             emailGenerator: "https://flynn.boolean.careers/exercises/api/random/mail",
             emails: [], 
             errorMessage: null, 
+            allEmailGenerated: false,  
         }
     },
     methods: { 
         generateEmail() {
-        for (let i = 0; i < 10; i++) {
+        for (let i = 0; i < this.numberOfEmail; i++) {
             axios
                 .get(this.emailGenerator)
                 .then(response => {
-                    this.emails.push(response.data.response)
+                    this.emails.push(response.data.response); 
+                    if (i === this.numberOfEmail - 1) {
+                        allEmailGenerated = true
+                        
+                    }
                 }
                 )
                 .catch(error => {
