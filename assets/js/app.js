20408859e485c6d86338ed27e41e8eae8f2569ed
giuseppevignanello <peppe.vignanello@gmail.com>
2023-04-11 15:26:35 +0200

    create a single email

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,10 +3,14 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-      
+          emailGenerator : "https://flynn.boolean.careers/exercises/api/random/mail",
+          randomEmail: null
       }
     },
     mounted() {
-       
+        axios
+        .get(this.emailGenerator)
+        .then(response =>
+           this.randomEmail = response.data.response)
     }
   }).mount('#app')
